cc974b0425a5112a4f01d1ffa8afc41a4856f8f8
ktcl <ktcl444@gmail.com>
2019-12-27 19:11:44 +0800

    约瑟夫环

--- algorithm/ysfh.php
+++ algorithm/ysfh.php
@@ -1,33 +1,12 @@
 <?php
-//function ysfh($num, $no)
-//{
-//    $p = 0;
-//    for ($i = 2; $i <= $num; $i++) {
-//        $p = intval($p + $no) % $i;
-//    }
-//
-//    return $p + 1;
-//}
-//
-//echo ysfh(3, 2);
+
 
 /***
- * @param $sum string 总人数
+ * @param $sum int 总人数
  * @param $value int 报数
  * @param $n int 轮次
  * @return int
  */
-//function ysfdg($sum, $value, $n)
-//{
-//    if ($n == 1)
-//        return (bcadd($sum, $value) - 1) % $sum;
-//    else
-//        return (ysfdg($sum - 1, $value, $n - 1) + $value) % $sum;
-//}
-//
-//echo ysfdg(10,4,10);
-
-
 function test($sum, $value, $n)
 {
     if ($n == 1) {
@@ -37,5 +16,4 @@ function test($sum, $value, $n)
     }
 }
 
-echo test(10, 4, 10);
-?>
+echo test(10, 4, 10); //10个人 第4个跳海 第10轮跳海的人最开始是多少号
