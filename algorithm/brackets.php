ee0e2ad8915c92a1acb89e679f8bebd869b47e5c
ktcl <ktcl444@gmail.com>
2019-12-30 07:29:00 +0800

    brackets modify

--- algorithm/brackets.php
+++ algorithm/brackets.php
@@ -7,20 +7,23 @@ function check_brackets($str)
     $str_len = strlen($str);
     $temp = [];
     $brackets = [];
+    $has_left_bracket = false;
     for ($i = 0; $i < $str_len; $i++) {
         $char = $str[$i];
         if (in_array($char, $left_brackets)) {
             array_push($temp, array($char, $i));
+            $has_left_bracket = true;
         } else if (in_array($char, $right_brackets) && ($temp && brackets[$char] == end($temp)[0])) {
             $brackets[] = array(end($temp)[1] . ' : ' . brackets[$char], "$i : " . $char);
             array_pop($temp);
         }
     }
-    $result = empty($temp);
+    $result = empty($temp) && $has_left_bracket;
     $result && print_r($brackets);
     return $result;
 }
 
-$str = '[](({[]}))';
+//$str = '[](({[]}))';
+$str = '[]';
 echo check_brackets($str) ? 'yes' : 'no';
 ?>
