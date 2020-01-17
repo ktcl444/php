bb276b847904cddbec8cc421cc3f9d59d72b9d8b
kongy <7336036@163.com>
2020-01-17 20:31:11 +0800

    汉明距离 算法优化

--- algorithm/hmjl.php
+++ algorithm/hmjl.php
@@ -14,17 +14,26 @@
         }
         return $result;
     }
-	
+/* 	解法一：可以通过按位与操作，通过将每一位和1与操作来求出1的个数。
+解法二（最优解）：一个巧妙的方法，一个不为0的二进制数，肯定至少有一位是1，当这个数减一的时候，它的最后一位1会变为0，后边的所有0会变为1。比如10100，减一之后会变为10011，然后用原数字10100和10011进行与操作之后，会得到10000，也就是通过这个操作，可以将一个1变为0，所以一个二进制数字能进行多少次这样的操作，就有多少个1. */
 function test($n,$flag)
 {
- $count = 0;
+/* 	 $count = 0;
+  $flag = 1;
   while ($flag != 0) {
    if (($n & $flag) != 0) {
     $count++;
    }
    $flag = $flag << 1;
   }
-  return $count;
+  return $count; */
+    $count = 0;
+ while($n != 0){
+  $count++;
+  $n = $n & ($n-1);
+ }
+ return $count;
+}
 }
 
     function getBinaryNum($x)
