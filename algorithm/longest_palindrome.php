83308afe21d0ae7898f59f2591fc23aa6abaa668
kongy <7336036@163.com>
2020-04-02 09:37:20 +0800

    最长回文子串

--- algorithm/longest_palindrome.php
+++ algorithm/longest_palindrome.php
@@ -1,31 +1,36 @@
 <?php
 
 require_once 'base\AlgorithmBase.php';
-
-//回文子串-中间向两边延伸(字母和字母中间的虚拟位置作为中点)
+//最长回文子串-中间向两边延伸(字母和字母中间的虚拟位置作为中点)
 class Solution extends \algorithm\base\AlgorithmBase
 {
-    function countSubstrings($s)
+    function longestPalindrome($s)
     {
-        $result = 0;
+		if(empty($s)) return '';
+		
         $length = strlen($s);
-
+		$start = 0;
+		$end = 0;
         for ($center = 0; $center <= 2 * $length - 1; $center++) {
             $left = $center / 2;
             $right = $left + $center % 2;
             while ($left >= 0 && $right < $length && substr($s, $left, 1) == substr($s, $right, 1)) {
-                $result++;
+				if($right - $left > $end - $start)
+				{
+					$start = $left;
+					$end = $right;
+				}
                 $left--;
                 $right++;
             }
         }
-        return $result;
+        return substr($s,$start,$end - $start + 1);
     }
 
     function test()
-    {
-        print_r($this->countSubstrings('abc'));
-        print_r($this->countSubstrings('aaa'));
+    { print_r($this->longestPalindrome('cbbd'));
+        //print_r($this->longestPalindrome('abc'));
+        //print_r($this->longestPalindrome('aaa'));
     }
 }
 
