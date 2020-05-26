d05aaf0ce413e54da059ee57d67e13f43565cdc7
kongy <7336036@163.com>
2020-05-26 19:51:36 +0800

    解码方法

--- algorithm/num_decodings.php
+++ algorithm/num_decodings.php
@@ -8,16 +8,15 @@ class Solution extends \algorithm\base\AlgorithmBase
 	function numDecodings($s){
 		$len = strlen($s);
 		$dp[$len] = 1;
-		if($s[$len-1] != 0)$dp[$len - 1] = 1;
-		for($i = $len - 2;$i >= 0;$i--){
+		for($i = $len - 1;$i >= 0;$i--){
 			if($s[$i] == 0){
 				$dp[$i] = 0;
 			}else{
-				$dp[$i] = $dp[$i+1] + ($s[$i] * 10 + $s[$i + 1] <= 26 ? $dp[$i+2]:0);
+				$dp[$i] = $dp[$i+1] + ($s{$i} * 10 + $s{$i + 1} <= 26 ? $dp[$i+2]:0);
 			}
 				
 		}
-		return $dp[0] ?? 0;
+		return $dp[0];
 	}
 	
     function test()
